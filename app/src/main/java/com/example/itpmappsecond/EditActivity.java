00ec9c5ecc046d291e7e1002c682defed9a4ecc3
koya-hikigi <hikigi@itpm-gk.com>
2019-12-30 19:30:19 +0900

    第5回完了

--- app/src/main/java/com/example/itpmappsecond/EditActivity.java
+++ app/src/main/java/com/example/itpmappsecond/EditActivity.java
@@ -1,5 +1,7 @@
 package com.example.itpmappsecond;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    private static final String KEY_TITLE = "key_title";
+    private EditText mTitleEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class EditActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mTitleEditText = findViewById(R.id.titleEditText);
+
+        String title = getIntent().getStringExtra(KEY_TITLE);
+        if (title != null){
+            mTitleEditText.setText(title);
+        } else {
+            mTitleEditText.setText("");
+        }
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +41,12 @@ public class EditActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public static Intent createIntent(Context context, String title){
+        Intent intent = new Intent(context, EditActivity.class);
+        intent.putExtra(KEY_TITLE, title);
+        return intent;
     }
 
 }
--- app/src/main/java/com/example/itpmappsecond/MainActivity.java
+++ app/src/main/java/com/example/itpmappsecond/MainActivity.java
@@ -9,12 +9,27 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // 1.ListViewをレイアウトファイルから読み込む
+        mListView = findViewById(R.id.main_list);
+
+        // 2.Adapterを作成する
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+
+        // 3.ListViewにAdapterをセットする
+        mListView.setAdapter(mAdapter);
+
+        // 通常のクリックリスナーのセット
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.d(MainActivity.class.getSimpleName(), position + "番目のリストアイテムが押されました");
+                String title = String.valueOf(adapterView.getItemAtPosition(position));
+                Intent intent = EditActivity.createIntent(MainActivity.this, title);
+                startActivity(intent);
+            }
+        });
+
+        // 長押しでのクリックリスナーのセット
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.d(MainActivity.class.getSimpleName(), position + "番目のリストアイテムが長押しされました");
+                String title = String.valueOf(adapterView.getItemAtPosition(position));
+                mAdapter.remove(title);
+                Toast.makeText(MainActivity.this, title +"を削除しました", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,4 +84,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // リスト表示用メソッド
+    private void displayDataList(){
+
+        // 1.アダプター内のデータをリセットする
+        mAdapter.clear();
+
+        // 2.新しいデータをアダプターに設定する(セットする)
+        List<String> dataList =  Arrays.asList("ホーム", "事業内容", "企業情報", "採用情報", "お問い合わせ");
+        mAdapter.addAll(dataList);
+
+        // 3.アダプターにデータが通知されたことを通知する
+        mAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayDataList();
+    }
 }
