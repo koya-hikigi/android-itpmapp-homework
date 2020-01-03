8a98b81b34a0d87767e2409ada6dfd36c20707bf
koya-hikigi <hikigi@itpm-gk.com>
2020-01-03 13:35:09 +0900

    第6回完了

---
+++ app/src/main/java/com/example/itpmappsecond/pojo/TitleDataItem.java
@@ -0,0 +1,29 @@
+package com.example.itpmappsecond.pojo;
+
+public class TitleDataItem {
+
+    private int id;
+    private String title;
+
+    public TitleDataItem(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
